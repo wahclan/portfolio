1f32ecdcd5178f6e436c2b53ed93d4b758c1390a
samirmmubituzzaman5@gmail.com <samirmmubituzzaman5@gmail.com>
2024-02-15 21:35:52 -0500

    main

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Samir &nbsp;
-            <span className='sm:block hidden'> |  AI Researche</span>
+            <span className='sm:block hidden'> |  AI Research</span>
           </p>
         </Link>
 
