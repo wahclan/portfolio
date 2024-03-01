fc2eb1b6148755483502a7ddbca7eec934818ab8
samir M <68399535+wahclan@users.noreply.github.com>
2024-02-29 19:23:57 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Samir</span>
+            Hi, I'm <span className='text-[#915EFF]'>Samir Mubituzzaman</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop AI models <br className='sm:block hidden' />
